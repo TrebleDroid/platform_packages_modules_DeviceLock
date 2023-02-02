b7874c0c1387d1b6cc2327c4a90128956ba78c06
Amos Bianchi <amosbianchi@google.com>
2023-02-02 09:08:15 -0800

    DLC: Download kiosk APK from URL.
    Bug: 266716782
    Test: atest DeviceLockControllerRoboTests
    
    Change-Id: I1826c339379ef84a6efd7af354cb812a4dae65c0

---
+++ DeviceLockController/src/com/android/devicelockcontroller/policy/DownloadRetryPolicy.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.devicelockcontroller.policy;
+
+/** Retry policy for {@link DownloadPackageTask}. */
+public interface DownloadRetryPolicy {
+    /**
+     * Checks if allowed to try download again when the previous request failed.
+     *
+     * @return true if need to try again, false if not
+     */
+    boolean needToRetry();
+
+    /** Returns how many retry attempts have been made. */
+    int getCurrentRetryCount();
+}
