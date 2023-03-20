6e8710b7d027cae2e8a7acc67babe997d49c6c6d
Yanli Wan <yanliwan@google.com>
2023-03-20 23:10:11 +0000

    Add the activity used in deferred setup provisioning flow
    Bug: 272497885
    Test: build & test locally
    Change-Id: Id04cd95518638ff7aab92abf9c33d2db8da80d8b

--- DeviceLockController/src/com/android/devicelockcontroller/activities/ProvisionInfoFragment.java
+++ DeviceLockController/src/com/android/devicelockcontroller/activities/ProvisionInfoFragment.java
@@ -16,11 +16,14 @@
 
 package com.android.devicelockcontroller.activities;
 
+import static com.android.devicelockcontroller.common.DeviceLockConstants.ACTION_START_DEVICE_FINANCING_DEFERRED_PROVISIONING;
+
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -33,44 +36,74 @@ import com.android.devicelockcontroller.R;
 import com.android.devicelockcontroller.setup.SetupParameters;
 import com.android.devicelockcontroller.util.LogUtil;
 
+import java.util.Objects;
+
 /**
  * The screen that provides information about the provision.
  */
 public final class ProvisionInfoFragment extends Fragment {
 
     private static final String TAG = "ProvisionInfoFragment";
+
     @Nullable
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater,
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_provision_info, container, false);
-        TextView headerTextView = view.findViewById(R.id.header_text);
-        String providerName = SetupParameters.getKioskAppProviderName(getActivity());
-        if (TextUtils.isEmpty(providerName)) {
-            LogUtil.e(TAG, "Device provider name is empty, should not reach here.");
-            return view;
-        }
-        if (headerTextView == null) {
-            LogUtil.e(TAG, "Could not find header TextView, should not reach here.");
-            return view;
-        }
-        headerTextView.setText(getString(R.string.device_provided_by_creditor, providerName));
-
-        return view;
+        return inflater.inflate(R.layout.fragment_provision_info, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_provision_info);
-        ProvisionInfoListAdapter adapter = new ProvisionInfoListAdapter();
+        String providerName = SetupParameters.getKioskAppProviderName(getActivity());
+        if (TextUtils.isEmpty(providerName)) {
+            LogUtil.e(TAG, "Device provider name is empty, should not reach here.");
+            return;
+        }
 
-        ProvisionInfoViewModel viewModel = new ViewModelProvider(this).get(
-                ProvisionInfoViewModel.class);
-        viewModel.getProvisionInfoListLiveData().observe(getViewLifecycleOwner(),
+        ProvisionInfoViewModel viewModel;
+        if (ACTION_START_DEVICE_FINANCING_DEFERRED_PROVISIONING.equals(
+                Objects.requireNonNull(getActivity()).getIntent().getAction())) {
+            viewModel = new ViewModelProvider(this).get(
+                    DeviceFinancingDeferredProvisionInfoViewModel.class);
+        } else {
+            viewModel = new ViewModelProvider(this).get(
+                    DeviceFinancingProvisionInfoViewModel.class);
+        }
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview_provision_info);
+        if (recyclerView == null) {
+            LogUtil.e(TAG, "Could not find provision info RecyclerView, should not reach here.");
+            return;
+        }
+        ProvisionInfoListAdapter adapter = new ProvisionInfoListAdapter();
+        viewModel.mProvisionInfoListLiveData.observe(getViewLifecycleOwner(),
                 adapter::submitList);
         recyclerView.setAdapter(adapter);
+        ImageView imageView = view.findViewById(R.id.header_icon);
+        if (imageView == null) {
+            LogUtil.e(TAG, "Could not find header ImageView, should not reach here.");
+            return;
+        }
+        viewModel.mHeaderDrawableIdLiveData.observe(getViewLifecycleOwner(),
+                imageView::setImageResource);
+
+        TextView headerTextView = view.findViewById(R.id.header_text);
+        if (headerTextView == null) {
+            LogUtil.e(TAG, "Could not find header TextView, should not reach here.");
+            return;
+        }
+        viewModel.mHeaderTextIdLiveData.observe(getViewLifecycleOwner(),
+                id -> headerTextView.setText(getString(id, providerName)));
+
+        TextView subheaderTextView = view.findViewById(R.id.subheader_text);
+        if (subheaderTextView == null) {
+            LogUtil.e(TAG, "Could not find subheader TextView, should not reach here.");
+            return;
+        }
+        viewModel.mSubheaderTextIdLiveData.observe(getViewLifecycleOwner(),
+                id -> subheaderTextView.setText(getString(id, providerName)));
     }
 }
