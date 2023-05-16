5e2f25f09940ef7e104cc96b2e1cccb586352d97
Amos Bianchi <amosbianchi@google.com>
2023-05-16 14:56:56 -0700

    DLC: Disallow clearing controller storage.
    Bug: 279504169
    Test: pm clear throws exception
    
    Change-Id: If7675aec5146bf9778fb46395ad584aa3ae62428

--- DeviceLockController/src/com/android/devicelockcontroller/DeviceLockControllerApplication.java
+++ DeviceLockController/src/com/android/devicelockcontroller/DeviceLockControllerApplication.java
@@ -35,6 +35,8 @@ import com.android.devicelockcontroller.policy.SetupControllerImpl;
 import com.android.devicelockcontroller.policy.TaskWorkerFactory;
 import com.android.devicelockcontroller.util.LogUtil;
 
+import com.google.common.util.concurrent.MoreExecutors;
+
 /**
  * Application class for Device Lock Controller.
  */
@@ -42,7 +44,6 @@ public class DeviceLockControllerApplication extends Application implements
         PolicyObjectsInterface, Configuration.Provider {
     private static final String TAG = "DeviceLockControllerApplication";
 
-    private DeviceStateController mStateController;
     private DevicePolicyController mPolicyController;
 
     private static Context sApplicationContext;
@@ -53,24 +54,23 @@ public class DeviceLockControllerApplication extends Application implements
         super.onCreate();
         sApplicationContext = getApplicationContext();
         LogUtil.i(TAG, "onCreate");
+        // Make sure policies are enforced when the controller is started.
+        getStateController().enforcePoliciesForCurrentState().addListener(
+                () -> LogUtil.i(TAG, "Policies enforced"), MoreExecutors.directExecutor());
     }
 
     @Override
     @MainThread
     public DeviceStateController getStateController() {
-        if (mStateController == null) {
-            mStateController = new DeviceStateControllerImpl(this);
-        }
-
-        return mStateController;
+        return getPolicyController().getStateController();
     }
 
     @Override
     @MainThread
     public DevicePolicyController getPolicyController() {
         if (mPolicyController == null) {
-            final DeviceStateController stateController = getStateController();
-            mPolicyController = new DevicePolicyControllerImpl(this, stateController);
+            mPolicyController = new DevicePolicyControllerImpl(this,
+                    new DeviceStateControllerImpl(this));
         }
 
         return mPolicyController;
