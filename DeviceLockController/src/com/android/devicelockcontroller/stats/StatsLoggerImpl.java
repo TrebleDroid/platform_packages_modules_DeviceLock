e6704ba3f3a653ea98728ca805d7dfbf400d640b
Bojian Du <bojiandu@google.com>
2023-10-18 20:19:46 -0700

    [DLC vNext] Create StatsLogger helper class and add tests
    As discussed offline and in
    https://googleplex-android-review.git.corp.google.com/c/platform/packages/modules/DeviceLock/+/24931993/comment/6e634f44_4c3950d5/
    , adding this helper class to facilitate better testability. These
    tests now utilize a constructor that takes a StatsLogger object as a
    parameter, letting the tests to inject mocked object to the classes
    under test. Ideally, this should be done with a dependency injection
    framework like Dagger. But because we have not have that set up yet,
    I will add dependency injection as a separate task.
    
    The basic idea here is to separate the interaction with the
    auto-generated class from the Android dependencies, so in this way, we
    can test the StatsLogger class and its dependency (the auto-generated
    class) as a pure java class rather than having to utilize robolectric,
    which has been causing trouble for us due to its incompatibility with
    ExtendedMockito.
    
    Test: atest DeviceLockControllerRoboTests
    Bug: b/305984988, b/259302076
    Change-Id: I11a9cd0c363fe277a22ec7e332f9fee48419ed8f

---
+++ DeviceLockController/src/com/android/devicelockcontroller/stats/StatsLoggerImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.devicelockcontroller.stats;
+
+import static com.android.devicelockcontroller.DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__GET_DEVICE_CHECK_IN_STATUS;
+import static com.android.devicelockcontroller.DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__PAUSE_DEVICE_PROVISIONING;
+import static com.android.devicelockcontroller.DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__REPORT_DEVICE_PROVISIONING_COMPLETE;
+import static com.android.devicelockcontroller.DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__REPORT_DEVICE_PROVISION_STATE;
+
+import com.android.devicelockcontroller.DevicelockStatsLog;
+
+public final class StatsLoggerImpl implements StatsLogger{
+    @Override
+    public void logGetDeviceCheckInStatus() {
+        DevicelockStatsLog.write(DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED,
+                DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__GET_DEVICE_CHECK_IN_STATUS);
+    }
+
+    @Override
+    public void logPauseDeviceProvisioning() {
+        DevicelockStatsLog.write(DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED,
+                DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__PAUSE_DEVICE_PROVISIONING);
+    }
+
+    @Override
+    public void logReportDeviceProvisioningComplete() {
+        DevicelockStatsLog.write(DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED,
+                DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__REPORT_DEVICE_PROVISIONING_COMPLETE);
+    }
+
+    @Override
+    public void logReportDeviceProvisionState() {
+        DevicelockStatsLog.write(DevicelockStatsLog.DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED,
+                DEVICE_LOCK_CHECK_IN_REQUEST_REPORTED__TYPE__REPORT_DEVICE_PROVISION_STATE);
+    }
+}
